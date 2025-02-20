e5850a1bd445c1030fb5eae3c55757970eef1da7
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2025-02-20 21:37:08 +0100

    remove source file (just need a tangle)

--- lisp/init-applications.el
+++
@@ -1,9 +0,0 @@
-;;; init-applications.el -*- lexical-binding: t; -*-
-
-(use-package xdg-appmenu
-  :ensure (:host codeberg :repo "akib/emacs-xdg-appmenu")
-  :bind
-  (("C-<f2>" . xdg-appmenu)))
-
-(provide 'init-applications)
-;;; init-applications.el ends here
--- lisp/init-strokes.el
+++
@@ -1,9 +0,0 @@
-;;; init-strokes.el -*- lexical-binding: t; -*-
-
-;; set of function to bind to custom `strokes'
-(defun nto/jump-to-code ()
-  (interactive)
-  (dired "~/Code"))
-
-(provide 'init-strokes)
-;;; init-strokes.el ends here
